e7f0944e878829dbd8fb38a4e7e07b98e7a92a9b
evgeny <afazhenya2001@gmail.com>
2023-10-08 17:28:00 +0300

    check prod back url

--- packages/dashboard/src/main.tsx
+++ packages/dashboard/src/main.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './pages';
 
+console.log(import.meta.env);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />

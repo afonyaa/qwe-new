39d37ad0c28351d590b794a2d62d49be2eaef719
evgeny <afazhenya2001@gmail.com>
2023-10-19 13:30:22 +0300

    fix creds

--- packages/visitor-portal/src/modules/RedirectionProvider/index.tsx
+++ packages/visitor-portal/src/modules/RedirectionProvider/index.tsx
@@ -26,7 +26,7 @@ export const RedirectionProvider: FC<RedirectionProviderProps> = ({
   const shouldShowAuthForms = !isLoading && !data;
   const contextValue = {
     setCookie: (cookie: string) => {
-      document.cookie = `Authorization=${cookie}; domain=${COOKIE_DOMAIN} ${SECURE_FLAG}`;
+      document.cookie = `Authorization=${cookie}; domain=${COOKIE_DOMAIN}; ${SECURE_FLAG}`;
       refetch();
     },
   };

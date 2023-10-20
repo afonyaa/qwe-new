2fdd2ae6641347817bbe95a8223c65bf4ee4622e
evgeny <afazhenya2001@gmail.com>
2023-10-20 21:25:42 +0300

    empty quizzes

--- packages/dashboard/src/containers/Quizzes/QuizList/index.tsx
+++ packages/dashboard/src/containers/Quizzes/QuizList/index.tsx
@@ -3,14 +3,20 @@ import { FC } from 'react';
 import { QuizCard } from './QuizCard';
 import { QuizListProps } from './interfaces';
 import { Skeleton } from './Skeleton';
+import { NoQuizzes } from './NoQuizzes';
 
 export const QuizList: FC<QuizListProps> = ({ quizList, isLoading }) => {
   if (isLoading) {
     return <Skeleton />;
   }
+  quizList = [];
+
+  if (quizList?.length === 0) {
+    return <NoQuizzes />;
+  }
 
   return (
-    <div className="flex flex-col h-full gap-y-2 overflow-y-auto pr-4">
+    <div className="flex flex-col w-1/2 h-full gap-y-2 overflow-y-auto pr-4">
       {quizList?.map((quiz) => (
         <QuizCard
           quizName={quiz.name}
